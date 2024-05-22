8d378ede16b44212b4d61760372ad4ae1eb414ac
bagheera <r.boeran@gmail.com>
2024-05-22 16:19:31 +0700

    update

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,43 +1,43 @@
-package com.example;
+// package com.example;
 
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+// import static org.junit.jupiter.api.Assertions.assertFalse;
+// import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
+// /**
+//  * Unit test for simple App.
+//  */
+// public class AppTest 
+// {
    
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+//     @Test
+//     public void shouldAnswerWithTrue()
+//     {
+//         assertTrue( true );
+//     }
    
-    @Test
-    public void testIsEven1() {
-        boolean result1 = App.isEven(4);
-        assertTrue(result1);
+//     @Test
+//     public void testIsEven1() {
+//         boolean result1 = App.isEven(4);
+//         assertTrue(result1);
         
-        boolean result2 = App.isEven(7);
-        assertFalse(result2);
-    }
+//         boolean result2 = App.isEven(7);
+//         assertFalse(result2);
+//     }
     
-    // @Test
-    // public void testIsEven2(){
-    //     boolean result1=App.isEven(3);
-    //     assertTrue(result1);
+//     // @Test
+//     // public void testIsEven2(){
+//     //     boolean result1=App.isEven(3);
+//     //     assertTrue(result1);
        
-    // }
+//     // }
 
-    @Test
-    public void testConcatenateStrings() {
-        String result = App.concatenateStrings("Hello", "World");
-        assertEquals("HelloWorld", result);
-    }
-}
+//     @Test
+//     public void testConcatenateStrings() {
+//         String result = App.concatenateStrings("Hello", "World");
+//         assertEquals("HelloWorld", result);
+//     }
+// }
