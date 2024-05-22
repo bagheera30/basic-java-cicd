410d9bddff8cb36498d201610bc1119033ba2b57
bagheera <r.boeran@gmail.com>
2024-05-22 16:19:31 +0700

    continus inspeksion

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,16 +1,19 @@
 package com.example;
-
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-    }
+        Counter counter = new Counter();
+		
+		System.out.println("Current count:" + counter.getCount());
+		
+		counter.increment();
+		
+		System.out.println("Current count:" + counter.getCount());
+		
+		counter.decrement();
+		
+		System.out.println("Current count:" + counter.getCount());
 
-    public static boolean isEven(int number) {
-        return number % 2 == 0;
-    }
-
-    public static String concatenateStrings(String str1, String str2) {
-        return str1 + str2;
     }
 
 
